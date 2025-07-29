3880c0beac926ed0c092253adcf685f6721770fb
inesc986 <ines.catak@gmail.com>
2025-07-29 17:23:04 +0200

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/disponentkaines/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/disponentkaines/MainActivity.kt
@@ -2,5 +2,6 @@ package com.example.disponentkaines
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    // No need to override anything unless using plugins that require it
 }
